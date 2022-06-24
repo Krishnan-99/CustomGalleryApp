763c7530fb0edd657e404a878cb81fd1859a539d
Krishnan <krishnan.kannan99@yahoo.com>
2022-06-24 10:31:17 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/galleryapp/UI/Activity/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.galleryapp.UI.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.galleryapp.R
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
